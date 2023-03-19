f55af980cdd92ec59f395aff6d1db3950f3d1303
manjugoutham <gouthamy219@gmail.com>
2023-03-19 08:55:41 +0530

    Added : created contact addressbook

--- UC1.go
+++ UC1.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type Contact struct {
+	FirstName string
+	LastName  string
+	Address   string
+	City      string
+	State     string
+	Zip       string
+	Phone     string
+	Email     string
+}
+
 func main() {
-	fmt.Printf("Welcome to address book")
+	contact := Contact{
+		FirstName: "Goutham",
+		LastName:  "Y",
+		Address:   "RT Nagar Banglore",
+		City:      "Banglore",
+		State:     "Karnataka",
+		Zip:       "543211",
+		Phone:     "9876543211",
+		Email:     "goutham@gmail.com",
+	}
+
+	fmt.Printf("Created contact details are : %+v\n", contact)
 }
